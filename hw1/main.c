2d5d892b74d452b67d3dca97073d9a2a2ccd7ed5
Sepehr79 <sepehrmsm1379@gmail.com>
2021-02-24 10:03:05 +0330

    addNewCustomer function

--- hw1/main.c
+++ hw1/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+ * Enumerations
+ */
 enum Gender{
     MALE, FEMALE
 };
@@ -8,49 +11,76 @@ enum ProductType{
     FOOD, CLOTHING, CHEMICALS
 };
 
-struct Customer{
-    char *name;
-    char *lastName;
+/**
+ * Structs
+ */
+typedef struct{
+    char name[30];
+    char lastName[30];
     enum Gender gender;
     int age;
     int id;
-};
+}Customer;
 
-struct Product{
-    char *name;
+typedef struct{
+    char name[30];
     int price;
-    char *brand;
+    char brand[50];
     int id;
     enum ProductType type;
-};
+}Product;
 
-struct Sales{
+typedef struct{
     int customerId;
-    int productId[20];
+    int productsId[20];
     int maxPrice;
-};
+}Sales;
 
-static struct Customer customers[100];
+
+/**
+ * Prototype methods
+ */
+void addNewCustomer();
+void addNewProduct();
+
+/**
+ * Global values
+ */
+static Customer customers[100];
 static int CUSTOMERS_INDEX = 0;
 
-static struct Product products[100];
+static Product products[100];
 static int PRODUCTS_INDEX = 0;
 
-static struct Sales sales[100];
+static Sales sales[100];
 static int SALES_INDEX = 0;
 
-void addNewCustomer();
-
+/**
+ * Main method
+ */
 int main() {
+
+    printf("Enter your selection:\n");
+    printf("1. Add product\n");
+    printf("2. Add customer\n");
+    printf("3. Sale product\n");
+    printf("4. Show products\n");
+    printf("5. Show customers\n");
+    printf("6. Show sales\n");
+    printf("7. Show sales by id\n");
+    printf("8. Show sales by day\n");
+
+    printf("Your selection: ");
 
     int input;
     scanf("%d", &input);
 
     switch (input) {
         case 1:
-            addNewCustomer();
+            addNewProduct();
             break;
         case 2:
+            addNewCustomer();
             break;
         case 3:
             break;
@@ -67,20 +97,46 @@ int main() {
 
     }
 
-    printf("%s", customers[0].name);
+    printf("%s %s %d %d %d", customers[0].name, customers[0].lastName, customers[0].age, customers[0].id, customers[0].gender);
     return 0;
 }
 
+/**
+ * Functions
+ */
 void addNewCustomer() {
+    if (CUSTOMERS_INDEX < 100){
+        Customer customer;
 
-    struct Customer customer;
-    customer.name = "";
-    customer.lastName = "";
-    customer.age = 0;
+        printf("Enter customer name:");
+        scanf("%s", customer.name);
 
-    scanf("%s", customer.name);
+        printf("Enter customer lastName:");
+        scanf("%s", customer.lastName);
 
-    customers[CUSTOMERS_INDEX++] = customer;
+        printf("Enter customer age: ");
+        scanf("%d", &customer.age);
 
+        printf("Enter customer id: ");
+        scanf("%d", &customer.id);
+
+        printf("Select customer gender:\n");
+        printf("1. Male\n");
+        printf("2. Female\n");
+        printf("Your selection:");
+        
+
+        int input;
+        scanf("%d", &input);
+        if (input > 0 && input < 3){
+            customer.gender = input - 1;
+        }
+
+        customers[CUSTOMERS_INDEX++] = customer;
+    }
+
+}
+
+void addNewProduct() {
 
 }
